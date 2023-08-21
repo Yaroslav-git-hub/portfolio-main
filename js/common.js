eb944f180a7f93b8e63746b62b14c7d8db16e6c5
Yaroslav-git-hub <krive1ka.in0000@gmail.com>
2023-08-21 11:08:48 +0300

    new

---
+++ js/common.js
@@ -0,0 +1,40 @@
+const ruButton = document.querySelector('.header__translate--button-ru');
+const enButton = document.querySelector('.header__translate--button-en');
+const ruNav = document.querySelector('.header__nav-ru');
+const enNav = document.querySelector('.header__nav-en');
+const ruContactHeader = document.querySelector('.header__contact--button-ru')
+const enContactHeader = document.querySelector('.header__contact--button-en')
+
+enButton.addEventListener('click', () => {
+	ruNav.style.display = 'flex';
+	enNav.style.display = 'none';
+	ruButton.style.display = 'block';
+	enButton.style.display = 'none';
+	ruContactHeader.style.display = 'block';
+	enContactHeader.style.display = 'none';
+	});
+
+ruButton.addEventListener('click', () => {
+	ruNav.style.display = 'none';
+	enNav.style.display = 'flex';
+	ruButton.style.display = 'none';
+	enButton.style.display = 'block';
+	ruContactHeader.style.display = 'none';
+	enContactHeader.style.display = 'block';
+});
+
+const moonBttn = document.querySelector('.header__theme--button-moon');
+const sunBttn = document.querySelector('.header__theme--button-sun');
+const header = document.querySelector('.header')
+
+moonBttn.addEventListener('click', () => {
+	moonBttn.style.display = 'none';
+	sunBttn.style.display = 'block';
+	header.style.background = 'var(--bgsun)';
+});
+
+sunBttn.addEventListener('click', () => {
+	moonBttn.style.display = 'block';
+	sunBttn.style.display = 'none';
+	header.style.background = 'var(--bgmoon)';
+});
